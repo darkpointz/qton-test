80012af8437d42f28d7fe0da2e557ec63f526670
unknown <apointwithadarkspace@gmail.com>
2021-03-16 07:27:36 +0700

    firsttime

---
+++ functions/index.js
@@ -0,0 +1,182 @@
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+const app = require("express")();
+
+admin.initializeApp();
+
+const config = {
+  apiKey: "AIzaSyB_wMIbuBlzYRipSToCoUk_dvo6IzyXCo4",
+  authDomain: "project-qton-01.firebaseapp.com",
+  projectId: "project-qton-01",
+  storageBucket: "project-qton-01.appspot.com",
+  messagingSenderId: "127556982592",
+  appId: "1:127556982592:web:bfb5338c161779c1ff8ca5",
+  measurementId: "G-ZHJZ8K29SR",
+};
+
+const firebase = require("firebase");
+firebase.initializeApp(config);
+
+const db = admin.firestore();
+
+app.get("/getUser", (req, res) => {
+  db.collection("User")
+    .get()
+    .then((data) => {
+      let user = [];
+      data.forEach((doc) => {
+        user.push({
+          userId: doc.id,
+          username: doc.data().username,
+          password: doc.data().password,
+          firstname: doc.data().firstname,
+          lastname: doc.data().lastname,
+        });
+      });
+      return res.json(user);
+    })
+    .catch((err) => console.error(err));
+});
+
+const isEmail = (email) => {
+  const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (email.match(regEx)) return true;
+  else return false;
+};
+
+const isEmpty = (string) => {
+  if (string.trim() === "") return true;
+  else return false;
+};
+
+app.post("/signup", (req, res) => {
+  const newUser = {
+    email: req.body.email,
+    password: req.body.password,
+    confirmPassword: req.body.confirmPassword,
+    firstname: req.body.firstname,
+    lastname: req.body.lastname,
+    handle: req.body.handle,
+  };
+
+  let errors = {};
+
+  if (isEmpty(newUser.email)) {
+    errors.email = "Must not be empty";
+  } else if (!isEmail(newUser.email)) {
+    errors.email = "Must be a valid email address";
+  }
+
+  if (isEmpty(newUser.password)) errors.password = "Must not be empty";
+  if (newUser.password !== newUser.confirmPassword)
+    errors.confirmPassword = "Passwords must match";
+  if (isEmpty(newUser.handle)) errors.handle = "Must not be empty";
+
+  if (Object.keys(errors).length > 0) return res.status(400).json(errors);
+
+  let token, userId;
+  db.doc(`/user/${newUser.handle}`)
+    .get()
+    .then((doc) => {
+      if (doc.exists) {
+        return res.status(400).json({ handle: "this handle is already taken" });
+      } else {
+        return firebase
+          .auth()
+          .createUserWithEmailAndPassword(newUser.email, newUser.password);
+      }
+    })
+    .then((data) => {
+      userId = data.user.uid;
+      return data.user.getIdToken();
+    })
+    .then((idToken) => {
+      token = idToken;
+      const userCredentials = {
+        handle: newUser.handle,
+        email: newUser.email,
+        createdAt: new Date().toISOString(),
+        userId,
+        firstname: newUser.firstname,
+        lastname: newUser.lastname,
+      };
+      return db.doc(`/users/${newUser.handle}`).set(userCredentials);
+    })
+    .then(() => {
+      return res.status(201).json({ token });
+    })
+    .catch((err) => {
+      console.error(err);
+      if (err.code === "auth/email-already-in-use") {
+        return res.status(400).json({ email: "Email is already in use" });
+      } else {
+        return res.status(500).json({ error: err.code });
+      }
+    });
+});
+
+app.get("/signupGoogle", (res, req) => {
+  const provider = new firebase.auth.GoogleAuthProvider();
+
+  firebase
+    .auth()
+    .signInWithPopup(provider)
+    .then(function (authData) {})
+    .catch(function (error) {
+      console.error(error);
+    });
+});
+
+app.post("/login", (req, res) => {
+  const user = {
+    email: req.body.email,
+    password: req.body.password,
+  };
+
+  let errors = {};
+
+  if (isEmpty(user.email)) errors.email = "Must not be empty";
+  if (isEmpty(user.password)) errors.password = "Must not be empty";
+
+  if (Object.keys(errors).length > 0) return res.status(400).json(errors);
+
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(user.email, user.password)
+    .then((data) => {
+      return data.user.getIdToken();
+    })
+    .then((token) => {
+      return res.json({ token });
+    })
+    .catch((err) => {
+      console.error(err);
+      if (err.code === "auth/wrong-password") {
+        return res
+          .status(403)
+          .json({ general: "Wrong credentials, please try again" });
+      }
+      return res.status(500).json({ error: err.code });
+    });
+});
+
+app.post("/Add", (req, res) => {
+  const student = {
+    Student: admin.firestore.FieldValue.arrayUnion({
+      name: req.body.name,
+      id: req.body.id,
+    }),
+  };
+  db.collection("User")
+    .doc("h0i3RHA93uzckH0ewzFU")
+    .update(student)
+    .then((doc) => {
+      res.json({ message: "Add student" });
+    })
+    .catch((err) => {
+      res.status(500).json({ error: "something went wrong" });
+      console.error(err);
+    });
+});
+
+exports.api = functions.https.onRequest(app);
